66cc787d2ec7055c442b02cf2decf8e376aeb0ca
AndjelaJ <AndjelaJ@ANDJELAJW10-NB>
2021-10-21 18:16:19 +0200

    Add project files.

---
+++ Klasa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KompleksniBroj
+{
+    class KompleksniBrojKlasa
+    {
+        public double RealniDio;
+        public double ImaginarniDio;
+
+        public KompleksniBrojKlasa(double realni, double imaginarni)
+        {
+            RealniDio = realni;
+            ImaginarniDio = imaginarni;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} + {1}i", RealniDio, ImaginarniDio);
+        }
+    }
+}
---
+++ Struktura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KompleksniBroj
+{
+    struct KompleksniBrojStruktura
+    {
+        public double RealniDio;
+        public double ImaginarniDio;
+        
+        public KompleksniBrojStruktura(double realni, double imaginarni)
+        {
+            RealniDio = realni;
+            ImaginarniDio = imaginarni;
+        }
+
+        public override string ToString()
+        {
+            return $"({RealniDio}, {ImaginarniDio})";
+        }
+    }
+}
